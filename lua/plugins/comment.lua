f9ed3b51fafc04c1991eebdabdfd16152b050fcc
Sam K <33263129+samk-dev@users.noreply.github.com>
2023-08-20 23:18:39 +0200

    fix: now comments works in vue files

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,4 +1,17 @@
 return {
   "numToStr/Comment.nvim",
+  opts = {
+    opleader = {
+      ---Line-comment keymap
+      line = "//",
+      ---Block-comment keymap
+      block = "??",
+    },
+  },
+  config = function()
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    })
+  end,
   lazy = false,
 }
